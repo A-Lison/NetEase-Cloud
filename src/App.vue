147b9a7679bf66ba7bddad3b8fe74fcf8f155423
A-Lison <A-Lison@qq.com>
2021-07-31 23:36:30 +0800

    建立顶部框架

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
+    <top-nav></top-nav>
     <!-- <test></test> -->
-    <test></test>
     <!-- <router-view /> -->
   </div>
 </template>
 
 <script>
 import test from "./components/test.vue";
+import topNav from "./components/topNav.vue";
 
 export default {
   name: "App",
   components: {
     test,
+    topNav,
+  },
+  computed: {
+    height() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return 220;
+        case "sm":
+          return 400;
+        case "md":
+          return 500;
+        case "lg":
+          return 600;
+        case "xl":
+          return 800;
+      }
+    },
   },
 };
 </script>
---
+++ src/components/topNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top-nav">
+    <div class="top"></div>
+    <div class="buttom"></div>
+    <div class="test" :height="height"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "topNav",
+};
+</script>
+
+<style scoped>
+.top-nav .test {
+  width: 500px;
+  height: 200px;
+  background-color: pink;
+}
+</style>
